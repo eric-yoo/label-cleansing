45b3c3cc429545644fa51f50ddc2e1ff3ce5aeb8
Yong-Hwan Yoo <ajoiej729@gmail.com>
2021-07-10 02:25:34 +0900

    fix: add label book

--- evaluate.py
+++ evaluate.py
@@ -5,8 +5,8 @@ import json
 import sys
 
 directory = "submission"
-user_data = directory + "/train"
 valid_data = directory + "/val"
+test_data = "label_book" # this can be the label book, or any other test set you create
 n_epoch = 100
 
 ### DO NOT MODIFY BELOW THIS LINE, THIS IS THE FIXED MODEL ###
@@ -15,16 +15,6 @@ tf.random.set_seed(123)
 
 
 if __name__ == "__main__":
-    train = tf.keras.preprocessing.image_dataset_from_directory(
-        user_data,
-        labels="inferred",
-        label_mode="categorical",
-        class_names=["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x"],
-        shuffle=True,
-        seed=123,
-        batch_size=batch_size,
-        image_size=(32, 32),
-    )
 
     valid = tf.keras.preprocessing.image_dataset_from_directory(
         valid_data,
@@ -37,10 +27,17 @@ if __name__ == "__main__":
         image_size=(32, 32),
     )
 
-    total_length = ((train.cardinality() + valid.cardinality()) * batch_size).numpy()
-    if total_length > 10_000:
-        print(f"Dataset size larger than 10,000. Got {total_length} examples")
-        sys.exit()
+
+    test = tf.keras.preprocessing.image_dataset_from_directory(
+        test_data,
+        labels="inferred",
+        label_mode="categorical",
+        class_names=["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x"],
+        shuffle=False,
+        seed=123,
+        batch_size=batch_size,
+        image_size=(32, 32),
+    )
 
     base_model = tf.keras.applications.ResNet50(
         input_shape=(32, 32, 3),
@@ -63,22 +60,20 @@ if __name__ == "__main__":
         loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
         metrics=["accuracy"],
     )
-    model.summary()
     loss_0, acc_0 = model.evaluate(valid)
     print(f"loss {loss_0}, acc {acc_0}")
 
-    # train model
-    checkpoint = tf.keras.callbacks.ModelCheckpoint(
-        "ckpts/best_model",
-        monitor="val_accuracy",
-        mode="max",
-        save_best_only=True,
-        save_weights_only=True,
-    )
+    ## load the weight file directly
+    model.load_weights("ckpts/best_model")
+    loss, acc = model.evaluate(valid)
+    test_loss, test_acc = model.evaluate(test)
 
-    history = model.fit(
-        train,
-        validation_data=valid,
-        epochs=100,
-        callbacks=[checkpoint],
-    )
+    ## write result to file
+    with open('metrics.txt', 'w') as f:
+        f.write(f"final loss: {loss}\n")
+        f.write(f"final acc: {acc}\n")
+        f.write(f"test loss {test_loss}\n")
+        f.write(f"test acc {test_acc}")
+
+    with open('scores.json', "w") as fd:
+        json.dump({"test loss": test_loss, "test acc": test_acc}, fd, indent=4)
